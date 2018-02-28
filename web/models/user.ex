0d4554a98bc8ec34f83dbefe03bf3d1dce80a9a3
Karol M <dmuhafc@gmail.com>
2018-02-28 16:29:44 +0100

    Add User model with schema

---
+++ web/models/user.ex
@@ -0,0 +1,19 @@
+defmodule Discuss.User do
+  use Discuss.Web, :model
+
+  @required_params [:email, :provider, :token]
+
+  schema "users" do
+    field(:email, :string)
+    field(:provider, :string)
+    field(:token, :string)
+
+    timestamps()
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(@required_params)
+    |> validate_required(@required_params)
+  end
+end

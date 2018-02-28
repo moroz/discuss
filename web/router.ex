495ed29976c8d8254bcd719f206b66222ad90aed
Karol M <dmuhafc@gmail.com>
2018-02-28 11:27:48 +0100

    Add Topics with mechanisms to add them

--- web/router.ex
+++ web/router.ex
@@ -2,21 +2,24 @@ defmodule Discuss.Router do
   use Discuss.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", Discuss do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", TopicController, :index)
+    get("/topics/new", TopicController, :new)
+    post("/topics", TopicController, :create)
   end
 
   # Other scopes may use custom stacks.

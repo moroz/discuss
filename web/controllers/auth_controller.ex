f174c26aabadc17fe743601f0cf9472abb2e0f66
Karol M <dmuhafc@gmail.com>
2018-03-01 10:22:06 +0100

    Add ability to sign out

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -16,6 +16,12 @@ defmodule Discuss.AuthController do
 
   def callback(conn, _params), do: handle_error(conn)
 
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: topic_path(conn, :index))
+  end
+
   defp signin(conn, changeset) do
     case insert_or_update_user(changeset) do
       {:ok, user} ->
